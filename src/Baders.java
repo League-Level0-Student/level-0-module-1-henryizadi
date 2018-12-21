1c33b3b85b79c180340351be5154256629849785
henryizadi <league@WTS21.attlocal.net>
2018-12-20 16:58:01 -0800

    Badgersip my ages

---
+++ src/Baders.java
@@ -0,0 +1,14 @@
+
+public class Baders {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+System.out.print("Badger, ");
+
+		}
+		for (int i = 0; i < 2; i++) {
+			System.out.print("mushroom, ");
+
+					}
+
+	}
+}
