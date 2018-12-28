8ec5925d9e806dca9629e9a9412b47cacf579637
henryizadi <league@WTS21.attlocal.net>
2018-12-27 17:00:31 -0800

    Badgers and Christmas presents

--- src/Baders.java
+++ src/Baders.java
@@ -1,14 +1,21 @@
 
 public class Baders {
-	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-System.out.print("Badger, ");
 
+	public static void main(String[] args) {
+		for (int h = 0; h < 2; h++) {
+			for (int i = 0; i < 8; i++) {
+
+				System.out.print("Badger, ");
+
+			}
+			for (int i = 0; i < 2; i++) {
+				System.out.print("mushroom, ");
+			}
+			System.out.println();
+			
 		}
 		for (int i = 0; i < 2; i++) {
-			System.out.print("mushroom, ");
-
-					}
-
+			System.out.print("snake, ");
+		}
 	}
 }
---
+++ src/Christmaspresent.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Christmaspresent {
+public static void main(String[] args) {
+String first=JOptionPane.showInputDialog("What expensive toy did you get");
+if (first.equals(buggati)) {
+
+
+
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
